08e65fbed9e588abd78f32523d691c1c7aebfb6b
Joel <blandco@users.noreply.github.com>
2019-08-30 15:52:51 -0500

    Create esa-dark.php

---
+++ esa-dark.php
@@ -0,0 +1,18 @@
+<!-- Add these <style> tags to the <head> section of esa.php to enable dark mode! -->
+
+<style>
+body {background-color: #262626;}
+div {background-color: #262626; color: white;}
+h1   {color: white;}
+h2   {color: white;}
+h3   {color: white;}
+p    {color: white;}
+/* unvisited link */
+a:link {
+  color: white;
+}
+/* visited link */
+a:visited {
+  color: white;
+}
+</style>
